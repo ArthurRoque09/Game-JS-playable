9dd7af65a4abd245164d8d9a3c281ce6c53c84bf
ArthurRoque09 <arthurroque155@gmail.com>
2026-08-24 09:45:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import { MenuScene } from "./scenes/menuScene.js";
-import { GameScene } from "./scenes/gameScene.js";
-import { TutorialScene } from "./scenes/tutorialScene.js";
+import { MenuScene } from "./scenes/menuscene.js";
+import { GameScene } from "./scenes/gamescene.js";
+import { TutorialScene } from "./scenes/tutorialscene.js";
 
 const config = {
 
